26c2ef0d2aac56b860795b5a17d3a4883b2a34ff
Jason <azoulayjason@gmail.com>
2020-08-12 18:40:48 +0200

    script to generate feature

--- src/feature/index.ts
+++ src/feature/index.ts
@@ -1,10 +1,26 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicsException, url, apply, template, move, chain, mergeWith } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function feature(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+  
+  if (!_options.name || _options.name <= 0) {
+    throw new SchematicsException('You must provide a name');
+  }
+  const sourceTemplate = url('./files');
+
+
+  const sourceParametrizedTemplates = apply(sourceTemplate, [
+    template({
+      ..._options,
+      ...strings
+    }),
+    move(`projects/angular-ngrx-material-starter/src/app/core/data-access/business/${_options.name}/`)
+  ]);
+
+  return chain([
+    mergeWith(sourceParametrizedTemplates)
+  ]);
 }
